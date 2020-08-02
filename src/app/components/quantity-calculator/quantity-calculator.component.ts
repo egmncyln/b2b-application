1ded80243d4ceecbb73585bd412c3a8eea6becdf
Egemen Ceylan <egemen.ceylan1@gmail.com>
2020-08-02 23:15:57 +0300

    quantity-calculator comp is completed. app.comp.html is designed.

--- src/app/components/quantity-calculator/quantity-calculator.component.ts
+++ src/app/components/quantity-calculator/quantity-calculator.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-quantity-calculator',
   templateUrl: './quantity-calculator.component.html',
   styleUrls: ['./quantity-calculator.component.css']
 })
-export class QuantityCalculatorComponent implements OnInit {
+export class QuantityCalculatorComponent {
+  @Input() value: number = 1;
+
+  @Output() decreaseQuantity = new EventEmitter<void>();
+  @Output() increaseQuantity = new EventEmitter<void>();
 
   constructor() { }
 
-  ngOnInit(): void {
+  onDecreaseQuantityClicked() {
+    this.decreaseQuantity.emit();
   }
 
+  onIncreaseQuantityClicked() {
+    this.increaseQuantity.emit();
+  }
 }
