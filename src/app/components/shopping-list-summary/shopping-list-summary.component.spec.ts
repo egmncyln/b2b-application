0dc555af8e610044ffdfb8a17e2e4f86c89bc719
Egemen Ceylan <egemen.ceylan1@gmail.com>
2020-08-03 19:43:30 +0300

    Shopping-list-summary comp failed test is fixed

--- src/app/components/shopping-list-summary/shopping-list-summary.component.spec.ts
+++ src/app/components/shopping-list-summary/shopping-list-summary.component.spec.ts
@@ -1,25 +1,35 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ShoppingListSummaryComponent } from './shopping-list-summary.component';
+import { MockStore, provideMockStore } from '@ngrx/store/testing';
+import { AppState } from 'src/app/store/app.state';
+import { MemoizedSelector } from '@ngrx/store';
+import { Product } from 'src/app/models/product';
+import { shoppingListSelectors } from '../shopping-list/store/shopping-list.selectors';
 
 describe('ShoppingListSummaryComponent', () => {
-  let component: ShoppingListSummaryComponent;
-  let fixture: ComponentFixture<ShoppingListSummaryComponent>;
+    let component: ShoppingListSummaryComponent;
+    let fixture: ComponentFixture<ShoppingListSummaryComponent>;
+    let mockStore: MockStore<AppState>;
+    let mockSelector: MemoizedSelector<AppState, Product[]>;
+    const initialState = {};
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ ShoppingListSummaryComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            providers: [provideMockStore({ initialState })],
+            declarations: [ShoppingListSummaryComponent]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ShoppingListSummaryComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        mockStore = TestBed.inject(MockStore);
+        mockSelector = mockStore.overrideSelector(shoppingListSelectors.getShoppingList, []);
+        fixture = TestBed.createComponent(ShoppingListSummaryComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
